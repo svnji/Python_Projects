2f82fcc63f9d5829e10332dbf685427146bd8b4e
svnji <xsanji99@gmail.com>
2024-02-23 20:49:30 +0200

    just playing nothing new😁

--- PYTHON_ALL_PROJECTS/calc.py
+++ PYTHON_ALL_PROJECTS/calc.py
@@ -1,33 +1,38 @@
 from tkinter import *
 import tkinter.messagebox
 import math
+
 root = Tk()
 root.geometry("650x400+300+300")
 root.title("calculator")
 switch = None
-#calculator
+
+
+# calculator
 def calc():
-     op = input(" + , - , * , / ")
-     if op == "+":
-          num1 = int(input("num1: "))
-          num2 = int(input("num2: "))
-          result = num1 + num2
-          print(result)
-     elif op == "-":
-          num1 = int(input("num1: "))
-          num2 = int(input("num2: "))
-          result = num1 - num2
-          print(result)
-     elif op == "*":
-          num1 = int(input("num1: "))
-          num2 = int(input("num2: "))
-          result = num1 * num2
-          print(result)
-     elif op == "/":
-          num1 = int(input("num1: "))
-          num2 = int(input("num2: "))
-          result = num1 / num2
-          print(result)
-     print("(;")
+    op = input(" + , - , * , / ")
+    if op == "+":
+        num1 = int(input("num1: "))
+        num2 = int(input("num2: "))
+        result = num1 + num2
+        print(result)
+    elif op == "-":
+        num1 = int(input("num1: "))
+        num2 = int(input("num2: "))
+        result = num1 - num2
+        print(result)
+    elif op == "*":
+        num1 = int(input("num1: "))
+        num2 = int(input("num2: "))
+        result = num1 * num2
+        print(result)
+    elif op == "/":
+        num1 = int(input("num1: "))
+        num2 = int(input("num2: "))
+        result = num1 / num2
+        print(result)
+    print("(;")
+
+
 calc()
 root.mainloop()
--- PYTHON_ALL_PROJECTS/section.py
+++ PYTHON_ALL_PROJECTS/section.py
@@ -1,7 +1,30 @@
-n = int(input())
-groups = list(map(int, input().split()))
-count = [0, 0, 0, 0]
-for group in groups:
-    count[group - 1] += 1
-taxis = count[3] + count[2] + (count[1] * 2 + max(count[0] - count[2], 0) + 3) // 4
-print(taxis)
+degrees = list(map(float, input().split()))
+x = []
+for i in degrees:
+    if i >= 96:
+        x.append(4)
+    elif i >= 92:
+        x.append(3.7)
+    elif i >= 88:
+        x.append(3.4)
+    elif i >= 84:
+        x.append(3.2)
+    elif i >= 80:
+        x.append(3)
+    elif i >= 76:
+        x.append(2.8)
+    elif i >= 72:
+        x.append(2.6)
+    elif i >= 68:
+        x.append(2.4)
+    elif i >= 64:
+        x.append(2.2)
+    elif i >= 60:
+        x.append(2)
+    elif i >= 55:
+        x.append(1.5)
+    elif i >= 50:
+        x.append(1)
+    elif i < 50:
+        x.append(0)
+print(round(sum(x) / len(x), 2))
